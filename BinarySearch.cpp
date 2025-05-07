dfa439620c2858b22d3b5c18508612430c54613a
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-07 15:02:07 +0700

    menyempurnakan fungsi binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -83,3 +83,18 @@ void binarySearch()
             {
                 cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
                 return;
+    }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
